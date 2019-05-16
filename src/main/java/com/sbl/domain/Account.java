f6fc409a9fe3697396cceda66bbebfefcf9d8b7d
songbl <791243928@qq.com>
2019-05-16 15:16:27 +0800

    Initial commit

---
+++ src/main/java/com/sbl/domain/Account.java
@@ -0,0 +1,59 @@
+package com.sbl.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ */
+public class Account implements Serializable {
+
+    private Integer id;
+    private Integer uid;
+    private Double money;
+
+    //从表实体应该包含一个主表实体的对象引用
+    //多对一，多个账户属于一个用户，例如账户1.3都属于用户46
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", uid=" + uid +
+                ", money=" + money +
+                '}';
+    }
+}
